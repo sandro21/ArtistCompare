af92e6cac9dc70c632207ddd5db3ebf90a8e314b
Sandro <sandroqarqusashvili@gmail.com>
2025-08-07 18:53:59 -0400

    removed extra blur

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,11 @@ import Info from "../components/Info";
 import Streams from "../components/Streams";
 import RiaaCertifications from "../components/RiaaCertifications";
 import GlareHover from "../blocks/Animations/GlareHover/GlareHover";
-import ShapeBlur from "../blocks/Animations/ShapeBlur/ShapeBlur";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-20 pt-20">
-      <div style={{ position: 'relative', height: '500px', overflow: 'hidden', width: '100%' }}>
-        <ShapeBlur
-          variation={0}
-          pixelRatioProp={typeof window !== 'undefined' ? window.devicePixelRatio || 1 : 1}
-          shapeSize={0.5}
-          roundness={0.5}
-          borderSize={0.05}
-          circleSize={0.5}
-          circleEdge={1}
-        />
-        <div style={{ position: 'absolute', inset: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+  <div className="flex items-center justify-center w-full" style={{height: '500px'}}>
           <GlareHover
             glareColor="#ffffff"
             glareOpacity={0.3}
@@ -30,7 +19,6 @@ export default function Home() {
           >
             <Info />
           </GlareHover>
-        </div>
       </div>
       <Streams />
       <RiaaCertifications />
