af2579ba36e131545578b40da6eff28e336a2d8f
Sandro <33396533+sandro21@users.noreply.github.com>
2025-09-11 03:00:19 -0400

    Update SectionWrapper.tsx

--- components/SectionWrapper.tsx
+++ components/SectionWrapper.tsx
@@ -19,7 +19,7 @@ const SectionWrapper: React.FC<SectionWrapperProps> = ({
         background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 80%, rgba(65, 147, 105, 0.24) 110%)',
         boxShadow: '0 0 30px -17px rgba(56, 217, 134, 0.83) inset, 0 0 30px -8px rgba(56, 217, 134, 0.74)',
       }}
-      className={`px-3 py-6 sm:px-8 w-full max-w-3xl`}
+      className={`px-4 py-5 sm:px-8 w-full max-w-3xl`}
     >
       {header && (
         <div className={`font-bold text-2xl text-white text-center ${headerClassName ?? 'mb-6'}`}>
